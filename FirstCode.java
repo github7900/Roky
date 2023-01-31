dbe15739deb1b014107a03238d2ce5b3fe603386
MD. ABDUR RAHMAN ROKY <123226606+github7900@users.noreply.github.com>
2023-01-31 11:59:13 +0600

    Add files via upload

---
+++ FirstCode.java
@@ -0,0 +1,46 @@
+//abdur rahman roky
+/*  --------------------------- */
+public class FirstCode {
+    public static void main(String args[])
+    {
+        int n = 5;
+        float f = 5.4F;
+        char c = 'A';
+        boolean b = true;
+        String s = "ABCD";
+        System.out.println("The value of n is " + n);//printing the value of 'n'.
+        System.out.println("Hello World\n");//printing hello world.
+        if (n >= 10) {
+            System.out.println(n + "is greater then 10");
+        } else {
+            System.out.println(n + "is smaller then 10");
+        }
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Roky");
+            if (i == 4) {
+                break;
+            }
+        }
+    }
+}
+/*int arr[]={1,5,3,7,8};
+        System.out.println(arr.length);
+        System.out.println(arr[2]);
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+}*/
+/*
+int sum=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+            sum=sum+arr[i];
+        }
+        System.out.println(sum);
+ */
+/*
+String car[]={"Volvo", "BMW", "Ford", "Mazda"};
+ *
+ */
